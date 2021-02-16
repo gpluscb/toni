65133e4387fa8f2a5a2874283dc350ea96a413f7
MarRue <20143778+gpluscb@users.noreply.github.com>
2021-02-16 18:39:31 +0100

    rework random character command

--- src/main/java/com/github/gpluscb/smashggnotifications/command/game/RandomCharacterCommand.java
+++ src/main/java/com/github/gpluscb/smashggnotifications/command/game/RandomCharacterCommand.java
@@ -46,48 +46,73 @@ public class RandomCharacterCommand implements Command {
             if (game == CharacterTree.Game.BRAWL) stackZssSamus = true;
         }
 
-        if (argsSize > 1) {
-            String stackEchosString = args.get(1);
-            Boolean stackEchosTemp = MiscUtil.boolFromString(stackEchosString);
-            if (stackEchosTemp == null) {
-                ctx.reply("I don't understand if you want echos to be treated as one character or not. Please type either `true` or `false` for that argument.").queue();
-                return;
-            }
+        switch (game) {
+            case SMASH_64:
+                if (argsSize > 1) {
+                    tooManyArgs(ctx);
+                    return;
+                }
+                break;
+            case MELEE:
+                if (argsSize > 2) {
+                    tooManyArgs(ctx);
+                    return;
+                }
 
-            stackEchos = stackEchosTemp;
-        }
+                if (argsSize > 1) {
+                    Boolean stackSheikZeldaTemp = shouldStackX(ctx, args.get(1), "Sheik and Zelda");
+                    if (stackSheikZeldaTemp == null) return;
+                    stackSheikZelda = stackSheikZeldaTemp;
+                }
+                break;
+            case BRAWL:
+                if (argsSize > 3) {
+                    tooManyArgs(ctx);
+                    return;
+                }
 
-        if (argsSize > 2) {
-            String stackMiisString = args.get(2);
-            Boolean stackMiisTemp = MiscUtil.boolFromString(stackMiisString);
-            if (stackMiisTemp == null) {
-                ctx.reply("I don't understand if you want miis to be treated as one character or not. Please type either `true` or `false` for that argument.").queue();
-                return;
-            }
+                if (argsSize > 1) {
+                    Boolean stackSheikZeldaTemp = shouldStackX(ctx, args.get(1), "Sheik and Zelda");
+                    if (stackSheikZeldaTemp == null) return;
+                    stackSheikZelda = stackSheikZeldaTemp;
+                }
 
-            stackMiis = stackMiisTemp;
-        }
+                if (argsSize > 2) {
+                    Boolean stackZssSamusTemp = shouldStackX(ctx, args.get(2), "ZSS and Samus");
+                    if (stackZssSamusTemp == null) return;
+                    stackZssSamus = stackZssSamusTemp;
+                }
+                break;
+            case SMASH_4:
+                if (argsSize > 2) {
+                    tooManyArgs(ctx);
+                    return;
+                }
 
-        if (argsSize > 3) {
-            String stackSheikZeldaString = args.get(3);
-            Boolean stackSheikZeldaTemp = MiscUtil.boolFromString(stackSheikZeldaString);
-            if (stackSheikZeldaTemp == null) {
-                ctx.reply("I don't understand if you want Sheik and Zelda to be treated as one character or not. Please type either `true` or `false` for that argument.").queue();
-                return;
-            }
+                if (argsSize > 1) {
+                    Boolean stackMiisTemp = shouldStackX(ctx, args.get(1), "Miis");
+                    if (stackMiisTemp == null) return;
+                    stackMiis = stackMiisTemp;
+                }
+                break;
+            case ULTIMATE:
+                if (argsSize > 3) {
+                    tooManyArgs(ctx);
+                    return;
+                }
 
-            stackSheikZelda = stackSheikZeldaTemp;
-        }
+                if (argsSize > 1) {
+                    Boolean stackMiisTemp = shouldStackX(ctx, args.get(1), "Miis");
+                    if (stackMiisTemp == null) return;
+                    stackMiis = stackMiisTemp;
+                }
 
-        if (argsSize > 4) {
-            String stackZssSamusString = args.get(4);
-            Boolean stackZssSamusTemp = MiscUtil.boolFromString(stackZssSamusString);
-            if (stackZssSamusTemp == null) {
-                ctx.reply("I don't understand if you want Zero Suit Samus and Samus to be treated as one character or not. Please type either `true` or `false` for that argument.").queue();
-                return;
-            }
-
-            stackZssSamus = stackZssSamusTemp;
+                if (argsSize > 2) {
+                    Boolean stackEchosTemp = shouldStackX(ctx, args.get(2), "Echos");
+                    if (stackEchosTemp == null) return;
+                    stackEchos = stackEchosTemp;
+                }
+                break;
         }
 
         List<List<CharacterTree.Character>> possibleCharacters = characterTree.getAllCharacters(game, stackEchos, stackMiis, stackSheikZelda, stackZssSamus);
@@ -99,9 +124,24 @@ public class RandomCharacterCommand implements Command {
     }
 
     private void sendResponse(@Nonnull CommandContext ctx, @Nonnull List<CharacterTree.Character> selectedCharacters) {
-        String emotes = selectedCharacters.stream().map(character -> String.format("%s(%s)", MiscUtil.mentionEmote(character.getEmoteId()), character.getName())).collect(Collectors.joining("/"));
+        String emotes = selectedCharacters.stream()
+                .map(character -> String.format("%s(%s)", MiscUtil.mentionEmote(character.getEmoteId()), character.getName()))
+                .collect(Collectors.joining("/"));
 
         ctx.reply(emotes).queue();
+    }
+
+    private void tooManyArgs(@Nonnull CommandContext ctx) {
+        ctx.reply("Too many arguments. Use `toni, help random` for a detailed description.").queue();
+    }
+
+    @Nullable
+    private Boolean shouldStackX(@Nonnull CommandContext ctx, @Nonnull String arg, @Nonnull String name) {
+        Boolean stackX = MiscUtil.boolFromString(arg);
+        if (stackX == null)
+            ctx.reply(String.format("I don't understand if you want %s to be treated as one character or not. " +
+                    "Please type either `true` or `false` for that argument.", name)).queue();
+        return stackX;
     }
 
     @Nonnull
@@ -113,13 +153,18 @@ public class RandomCharacterCommand implements Command {
     @Nullable
     @Override
     public String getShortHelp() {
-        return "Picks a random character for you. Usage: `random [GAME] [TREAT ECHOS AS ONE?] [TREAT MIIS AS ONE?] [TREAT SHEIK/ZELDA AS ONE?] [TREAT SAMUS/ZSS AS ONE?]`";
+        return "Picks a random character for you. Usage: `random [GAME] [GAME SPECIFIC OPTIONS...]`";
     }
 
     @Nullable
     @Override
     public String getDetailedHelp() {
-        return "`random [GAME (64|melee|brawl|4|ult(default))] [TREAT ECHOS AS ONE CHARACTER? (true|false(default))] [TREAT MIIS AS ONE CHARACTER? (true|false(default))] [TREAT SHEIK/ZELDA AS ONE CHARACTER? (true(default for Melee/Brawl)|false(default for 4/Ultimate))] [TREAT ZERO SUIT SAMUS/SAMUS AS ONE CHARACTER? (true(default for Brawl)|false(default for 4/Ultimate))]`\n" +
+        return "`random` (default game is ult)\n" +
+                "`random 64`\n" +
+                "`random melee [TREAT SHEIK/ZELDA AS ONE CHARACTER? (true(default)|false)]`\n" +
+                "`random brawl [TREAT SHEIK/ZELDA AS ONE? (true(default)|false)] [TREAT SAMUS/ZSS AS ONE? (true(default)|false)]`\n" +
+                "`random 4 [TREAT MIIS AS ONE? (true|false(default))]`\n" +
+                "`random ult [TREAT MIIS AS ONE? (true|false(default))] [TREAT ECHOES AS ONE? (true|false(default))]`\n" +
                 "Selects a random character from the roster of a smash game.";
     }
 }

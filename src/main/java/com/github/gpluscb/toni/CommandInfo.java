a9eecac3ee4d86a629bab21b856857e8398a65ae
MarRue <20143778+gpluscb@users.noreply.github.com>
2021-09-22 20:07:03 +0200

    Introduce CommandInfo

---
+++ src/main/java/com/github/gpluscb/toni/CommandInfo.java
@@ -0,0 +1,135 @@
+package com.github.gpluscb.toni;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class CommandInfo {
+    private final boolean adminOnly;
+    @Nonnull
+    private final Permission[] requiredBotPerms;
+    @Nonnull
+    private final String[] aliases;
+    @Nonnull
+    private final CommandData commandData;
+    @Nullable
+    private final String shortHelp;
+    @Nullable
+    private final String detailedHelp;
+
+    public CommandInfo(boolean adminOnly, @Nonnull Permission[] requiredBotPerms, @Nonnull String[] aliases, @Nonnull CommandData commandData, @Nullable String shortHelp, @Nullable String detailedHelp) {
+        this.adminOnly = adminOnly;
+        this.requiredBotPerms = requiredBotPerms;
+        this.aliases = aliases;
+        this.commandData = commandData;
+        this.shortHelp = shortHelp;
+        this.detailedHelp = detailedHelp;
+    }
+
+    @Nonnull
+    public Permission[] getRequiredBotPerms() {
+        return requiredBotPerms;
+    }
+
+    public boolean isAdminOnly() {
+        return adminOnly;
+    }
+
+    @Nonnull
+    public String[] getAliases() {
+        return aliases;
+    }
+
+    @Nonnull
+    public CommandData getCommandData() {
+        return commandData;
+    }
+
+    /**
+     * @return null if this command should not be displayed by the category help command
+     */
+    @Nullable
+    public String getShortHelp() {
+        return shortHelp;
+    }
+
+    /**
+     * @return null if this command should not be displayed by the command specific help command
+     */
+    @Nullable
+    public String getDetailedHelp() {
+        return detailedHelp;
+    }
+
+    public static class Builder {
+        @Nullable
+        private Boolean adminOnly;
+        @Nullable
+        private Permission[] requiredBotPerms;
+        @Nullable
+        private String[] aliases;
+        @Nullable
+        private CommandData commandData;
+        @Nullable
+        private String shortHelp;
+        @Nullable
+        private String detailedHelp;
+
+        public Builder() {
+        }
+
+        @Nonnull
+        public Builder setAdminOnly(boolean adminOnly) {
+            this.adminOnly = adminOnly;
+            return this;
+        }
+
+        @Nonnull
+        public Builder setRequiredBotPerms(@Nonnull Permission[] requiredBotPerms) {
+            this.requiredBotPerms = requiredBotPerms;
+            return this;
+        }
+
+        @Nonnull
+        public Builder setAliases(@Nonnull String[] aliases) {
+            this.aliases = aliases;
+            return this;
+        }
+
+        @Nonnull
+        public Builder setCommandData(@Nonnull CommandData commandData) {
+            this.commandData = commandData;
+            return this;
+        }
+
+        /**
+         * null if this command should not be displayed by the category help command
+         */
+        @Nonnull
+        public Builder setShortHelp(@Nullable String shortHelp) {
+            this.shortHelp = shortHelp;
+            return this;
+        }
+
+        /**
+         * null if this command should not be displayed by the command specific help command
+         */
+        @Nonnull
+        public Builder setDetailedHelp(@Nullable String detailedHelp) {
+            this.detailedHelp = detailedHelp;
+            return this;
+        }
+
+        @Nonnull
+        public CommandInfo build() {
+            if (adminOnly == null || aliases == null || commandData == null || shortHelp == null || detailedHelp == null)
+                throw new IllegalStateException("All fields must be set");
+
+            if (requiredBotPerms == null) requiredBotPerms = new Permission[0];
+
+            return new CommandInfo(adminOnly, requiredBotPerms, aliases, commandData, shortHelp, detailedHelp);
+        }
+    }
+}
--- src/main/java/com/github/gpluscb/toni/command/Command.java
+++ src/main/java/com/github/gpluscb/toni/command/Command.java
@@ -1,11 +1,9 @@
 package com.github.gpluscb.toni.command;
 
-import net.dv8tion.jda.api.Permission;
+import com.github.gpluscb.toni.CommandInfo;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 // TODO: Neat system for getting arguments out of CommandContext with defaults optionals and such and such
 // Tried in the other branch but abandoned for now, seems not worth the effort
@@ -15,25 +13,5 @@ public interface Command {
     void execute(@Nonnull SlashCommandEvent event);
 
     @Nonnull
-    default Permission[] getRequiredBotPerms() {
-        return new Permission[0];
-    }
-
-    @Nonnull
-    String[] getAliases();
-
-    @Nonnull
-    CommandData getCommandData();
-
-    /**
-     * @return null if this command should not be displayed by the category help command
-     */
-    @Nullable
-    String getShortHelp();
-
-    /**
-     * @return null if this command should not be displayed by the command specific help command
-     */
-    @Nullable
-    String getDetailedHelp();
+    CommandInfo getInfo();
 }

d6e869b1ec64e0a0340f9ddffbe19aa8558eab0b
MarRue <20143778+gpluscb@users.noreply.github.com>
2021-10-06 19:17:59 +0200

    ruleset-id is not required

--- src/main/java/com/github/gpluscb/toni/command/game/CounterpickStagesCommand.java
+++ src/main/java/com/github/gpluscb/toni/command/game/CounterpickStagesCommand.java
@@ -168,7 +168,7 @@ public class CounterpickStagesCommand implements Command {
                 .setCommandData(new CommandData("counterpick", "Helps you through the stage ban/counterpick phase of a set")
                         .addOption(OptionType.USER, "banning-user", "The user banning the stage", true)
                         .addOption(OptionType.USER, "counterpicking-user", "The user counterpicking. This is yourself by default", false)
-                        .addOption(OptionType.INTEGER, "ruleset-id", "The ruleset id", true)) // Yea that feels unintuitive
+                        .addOption(OptionType.INTEGER, "ruleset-id", "The ruleset id", false)) // TODO: Yea that feels unintuitive
                 .build();
     }
 }

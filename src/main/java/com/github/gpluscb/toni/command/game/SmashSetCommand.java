54b7ebeaaa984ed9b0a570f8b26412a7b2a38731
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-01-21 23:35:13 +0100

    Ruleset selection for SmashSetCommand

--- src/main/java/com/github/gpluscb/toni/command/game/SmashSetCommand.java
+++ src/main/java/com/github/gpluscb/toni/command/game/SmashSetCommand.java
@@ -1,21 +1,29 @@
 package com.github.gpluscb.toni.command.game;
 
-import com.github.gpluscb.toni.command.Command;
-import com.github.gpluscb.toni.command.CommandContext;
-import com.github.gpluscb.toni.command.CommandInfo;
+import com.github.gpluscb.toni.command.*;
+import com.github.gpluscb.toni.command.menu.RulesetSelectMenu;
 import com.github.gpluscb.toni.command.menu.SmashSetMenu;
-import com.github.gpluscb.toni.util.MiscUtil;
-import com.github.gpluscb.toni.util.discord.ChannelChoiceWaiter;
-import com.github.gpluscb.toni.util.discord.menu.ActionMenu;
-import com.github.gpluscb.toni.util.discord.menu.TwoUsersChoicesActionMenu;
 import com.github.gpluscb.toni.smashset.Character;
 import com.github.gpluscb.toni.smashset.CharacterTree;
 import com.github.gpluscb.toni.smashset.Ruleset;
 import com.github.gpluscb.toni.smashset.SmashSet;
+import com.github.gpluscb.toni.util.MiscUtil;
+import com.github.gpluscb.toni.util.OneOfTwo;
+import com.github.gpluscb.toni.util.discord.ChannelChoiceWaiter;
+import com.github.gpluscb.toni.util.discord.menu.ActionMenu;
+import com.github.gpluscb.toni.util.discord.menu.TwoUsersChoicesActionMenu;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -39,9 +47,53 @@ public class SmashSetCommand implements Command {
     public void execute(@Nonnull CommandContext<?> ctx) {
         long user1 = 107565973652938752L;
         long user2 = 348127811275456512L;
-        Ruleset ruleset = rulesets.get(0);
+        Ruleset ruleset = null; // TODO: Default ruleset
         int firstToWhatScore = (3 + 1) / 2;
 
+        if (ruleset != null) {
+            startSmashSet(ruleset, ctx.getContext().mapT(MessageCommandContext::getMessage).mapU(SlashCommandContext::getEvent), firstToWhatScore, user1, user2);
+            return;
+        }
+
+        // TODO: Hella duped code
+        RulesetSelectMenu rulesetMenu = new RulesetSelectMenu(new RulesetSelectMenu.Settings.Builder()
+                .setActionMenuSettings(new ActionMenu.Settings.Builder()
+                        .setWaiter(channelWaiter.getEventWaiter())
+                        .setTimeout(15, TimeUnit.MINUTES)
+                        .build())
+                .setUser(ctx.getUser().getIdLong())
+                .setRulesets(rulesets)
+                .setStart(new MessageBuilder(String.format("%s, please select a ruleset.", ctx.getUser().getAsMention()))
+                        .mentionUsers(ctx.getUser().getIdLong())
+                        .build())
+                .setOnRulesetSelect((info, event) -> onRulesetSelect(info, event, firstToWhatScore, user1, user2))
+                .setOnTimeout(this::onRulesetSelectTimeout)
+                .build());
+
+        ctx.getContext()
+                .onT(msg -> rulesetMenu.displayReplying(msg.getMessage()))
+                .onU(slash -> rulesetMenu.displaySlashReplying(slash.getEvent()));
+    }
+
+    private synchronized void onRulesetSelect(@Nonnull RulesetSelectMenu.RulesetSelectionInfo info, @Nonnull SelectionMenuEvent event, int firstToWhatScore, long user1, long user2) {
+        event.deferEdit().queue();
+        startSmashSet(info.getSelectedRuleset(), OneOfTwo.ofT(event.getMessage()), firstToWhatScore, user1, user2);
+    }
+
+    private synchronized void onRulesetSelectTimeout(@Nonnull RulesetSelectMenu.RulesetSelectTimeoutEvent timeout) {
+        MessageChannel channel = timeout.getChannel();
+        if (channel == null) return;
+        if (channel instanceof TextChannel textChannel) {
+            if (!textChannel.getGuild().getSelfMember().hasPermission(textChannel, Permission.MESSAGE_HISTORY))
+                return;
+        }
+
+        channel.retrieveMessageById(timeout.getMessageId())
+                .flatMap(m -> m.editMessage("You didn't choose the ruleset in time.").setActionRows())
+                .queue(null, new ErrorHandler().ignore(ErrorResponse.UNKNOWN_MESSAGE));
+    }
+
+    private void startSmashSet(@Nonnull Ruleset ruleset, @Nonnull OneOfTwo<Message, SlashCommandEvent> replyTo, int firstToWhatScore, long user1, long user2) {
         SmashSetMenu menu = new SmashSetMenu(new SmashSetMenu.Settings.Builder()
                 .setTwoUsersChoicesActionMenuSettings(new TwoUsersChoicesActionMenu.Settings.Builder()
                         .setActionMenuSettings(new ActionMenu.Settings.Builder()
@@ -60,9 +112,9 @@ public class SmashSetCommand implements Command {
                 .setOnResult(this::onResult)
                 .build());
 
-        ctx.getContext()
-                .onT(msg -> menu.displayReplying(msg.getMessage()))
-                .onU(slash -> menu.displaySlashReplying(slash.getEvent()));
+        replyTo
+                .onT(menu::displayReplying)
+                .onU(menu::displaySlashReplying);
     }
 
     private void onResult(@Nonnull SmashSetMenu.SmashSetResult result, @Nonnull ButtonClickEvent event) {

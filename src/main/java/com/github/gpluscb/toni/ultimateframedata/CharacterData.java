65fbc3b6b2572f232453a1e972daae017121b9a4
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-02-10 20:22:43 +0100

    Fix character command

--- src/main/java/com/github/gpluscb/toni/ultimateframedata/CharacterData.java
+++ src/main/java/com/github/gpluscb/toni/ultimateframedata/CharacterData.java
@@ -1,23 +1,30 @@
 package com.github.gpluscb.toni.ultimateframedata;
 
+import com.google.gson.annotations.SerializedName;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public record CharacterData(@Nonnull String ufdUrl, @Nonnull String name,
-                            @Nonnull List<MoveSection> moveSections,
-                            @Nonnull com.github.gpluscb.toni.ultimateframedata.CharacterData.MiscData miscData) {
-    public record MoveSection(@Nonnull String sectionName, @Nonnull String htmlId,
+// TODO: the @SerializedName annotations might not be needed after gson allows for records natively
+public record CharacterData(@Nonnull @SerializedName("ufd_url") String ufdUrl, @Nonnull String name,
+                            @Nonnull @SerializedName("move_sections") List<MoveSection> moveSections,
+                            @Nonnull @SerializedName("misc_data") MiscData miscData) {
+    public record MoveSection(@Nonnull @SerializedName("section_name") String sectionName,
+                              @SerializedName("html_id") @Nonnull String htmlId,
                               @Nonnull List<MoveData> moves) {
     }
 
     public record MoveData(
             @Nonnull List<HitboxData> hitboxes,
-            @Nullable String moveName, @Nullable String startup, @Nullable String totalFrames,
-            @Nullable String landingLag, @Nullable String notes, @Nullable String baseDamage,
-            @Nullable String shieldLag, @Nullable String shieldStun,
-            @Nullable String whichHitbox, @Nullable String advantage,
-            @Nullable String activeFrames) {
+            @Nullable @SerializedName("move_name") String moveName, @Nullable String startup,
+            @Nullable @SerializedName("total_frames") String totalFrames,
+            @Nullable @SerializedName("landing_lag") String landingLag, @Nullable String notes,
+            @SerializedName("base_damage") @Nullable String baseDamage,
+            @Nullable @SerializedName("shield_lag") String shieldLag,
+            @SerializedName("shield_stun") @Nullable String shieldStun,
+            @Nullable @SerializedName("which_hitbox") String whichHitbox, @Nullable String advantage,
+            @Nullable @SerializedName("active_frames") String activeFrames) {
     }
 
     public record HitboxData(@Nullable String name, @Nonnull String url) {
@@ -25,15 +32,20 @@ public record CharacterData(@Nonnull String ufdUrl, @Nonnull String name,
 
     public record MiscData(@Nullable StatsData stats,
                            @Nonnull List<MoveData> moves,
-                           @Nonnull String htmlId) {
+                           @Nonnull @SerializedName("html_id") String htmlId) {
     }
 
     public record StatsData(@Nullable String weight, @Nullable String gravity,
-                            @Nullable String walkSpeed, @Nullable String runSpeed,
-                            @Nullable String initialDash, @Nullable String airSpeed,
-                            @Nullable String totalAirAcceleration, @Nullable String shFhShffFhffFrames,
-                            @Nullable String fallSpeedFastFallSpeed,
-                            @Nonnull List<String> oosOptions, @Nullable String shieldGrab,
-                            @Nullable String shieldDrop, @Nullable String jumpSquat) {
+                            @Nullable @SerializedName("walk_speed") String walkSpeed,
+                            @SerializedName("run_speed") @Nullable String runSpeed,
+                            @Nullable @SerializedName("initial_dash") String initialDash,
+                            @Nullable @SerializedName("air_speed") String airSpeed,
+                            @Nullable @SerializedName("total_air_acceleration") String totalAirAcceleration,
+                            @Nullable @SerializedName("sh_fh_shff_fhff_frames") String shFhShffFhffFrames,
+                            @Nullable @SerializedName("fall_speed_fast_fall_speed") String fallSpeedFastFallSpeed,
+                            @Nonnull @SerializedName("oos_options") List<String> oosOptions,
+                            @Nullable @SerializedName("shield_grab") String shieldGrab,
+                            @Nullable @SerializedName("shield_drop") String shieldDrop,
+                            @Nullable @SerializedName("jump_squat") String jumpSquat) {
     }
 }

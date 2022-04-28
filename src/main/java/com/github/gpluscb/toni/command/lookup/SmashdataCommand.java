ab55e46d42c4bcff73d621bc04a7728712d0e772
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-04-28 17:59:00 +0200

    Correctly make smasher default alias for SmashdataCommand

--- src/main/java/com/github/gpluscb/toni/command/lookup/SmashdataCommand.java
+++ src/main/java/com/github/gpluscb/toni/command/lookup/SmashdataCommand.java
@@ -211,7 +211,7 @@ public class SmashdataCommand implements Command {
         return new CommandInfo.Builder()
                 .setRequiredBotPerms(new Permission[]{Permission.MESSAGE_EMBED_LINKS})
                 .setAliases(new String[]{"smasher", "player", "smashdata", "data"})
-                .setShortHelp("Displays info about a smasher using data from [smashdata.gg](https://smashdata.gg). Usage: `smashdata <TAG...>`")
+                .setShortHelp("Displays info about a smasher using data from [smashdata.gg](https://smashdata.gg). Usage: `smasher <TAG...>`")
                 .setDetailedHelp("`smasher <TAG...>`\n" +
                         "Looks up a smash ultimate player by their tag using data from [smashdata.gg](https://smashdata.gg).\n" +
                         String.format("If there are multiple smashers matching the given tag, use the %s/%s to cycle through them.%n", Constants.ARROW_BACKWARD, Constants.ARROW_FORWARD) +
